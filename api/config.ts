ec6d47d0690cc91fb7d27e5b29571f6245044fcc
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-06-24 18:25:06 +0200

    Set default SPA and API client IDs in API config

--- api/config.ts
+++ api/config.ts
@@ -1,8 +1,10 @@
 export const config = {
   entraTenantId: setting("ENTRA_TENANT_ID", "972e8de4-e365-43a3-99ec-c86a0cc249e8"),
-  apiClientId: setting("API_CLIENT_ID"),
+  // Default to the Skunkworks API app registration if no environment override is supplied
+  apiClientId: setting("API_CLIENT_ID", "8b1e77b3-3017-4c54-8ab3-0e4864511b55"),
   apiClientSecret: setting("API_CLIENT_SECRET"),
-  spaClientId: setting("SPA_CLIENT_ID"),
+  // Default to the Skunkworks SPA app registration if no environment override is supplied
+  spaClientId: setting("SPA_CLIENT_ID", "21f093b0-e91a-4f62-ad71-2dee1e0cbc20"),
   graphTenantId: setting("GRAPH_TENANT_ID", "972e8de4-e365-43a3-99ec-c86a0cc249e8"),
   sharePointHostname: setting("SHAREPOINT_HOSTNAME"),
   sharePointSitePath: setting("SHAREPOINT_SITE_PATH", "/sites/InstructorPortal"),
