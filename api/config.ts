23f15e902a7ac1a489137532bb434e11c7e841df
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-07-05 09:28:54 +0200

    Use portal API app as default SPA client

--- api/config.ts
+++ api/config.ts
@@ -2,7 +2,7 @@ export const config = {
   entraTenantId: setting("ENTRA_TENANT_ID", "972e8de4-e365-43a3-99ec-c86a0cc249e8"),
   apiClientId: setting("API_CLIENT_ID", "8b1e77b3-3017-4c54-8ab3-0e4864511b55"),
   apiClientSecret: setting("API_CLIENT_SECRET"),
-  spaClientId: setting("SPA_CLIENT_ID", "21f093b0-e91a-4f62-ad71-2dee1e0cbc20"),
+  spaClientId: setting("SPA_CLIENT_ID", "8b1e77b3-3017-4c54-8ab3-0e4864511b55"),
   graphTenantId: setting("GRAPH_TENANT_ID", "972e8de4-e365-43a3-99ec-c86a0cc249e8"),
   sharePointHostname: setting("SHAREPOINT_HOSTNAME", "skunkworksacademy.sharepoint.com"),
   sharePointSitePath: setting("SHAREPOINT_SITE_PATH", "/sites/InstructorPortal"),
